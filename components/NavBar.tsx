9ddefeafffaaadf1a3e65c2d1595144e67f294d5
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-20 16:06:34 +0100

    added logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import { CustomButton } from '@components';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <header className="w-full absolute z-[10]">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            height={18}
+            width={118}
+            className="object-contain"
+          />
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
-export default NavBar
+export default NavBar;
