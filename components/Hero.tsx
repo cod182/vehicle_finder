1125d5ddc6715d2525d4812203bfa147a2c1258d
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-20 15:22:02 +0100

    created button componene

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import image from 'next/image';
+
+import { CustomButton } from '@components';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find | Book | Rent - Quick and easy</h1>
+        <p className="hero__subtitle">streamline your car rental experience</p>
+        <CustomButton />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
