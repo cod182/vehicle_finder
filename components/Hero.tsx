10d36b0e636768ddbce795b0f41e5eb2470179db
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-20 15:33:46 +0100

    added assets

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,8 @@
 'use client';
-
-import image from 'next/image';
+import Image from 'next/image';
 
 import { CustomButton } from '@components';
+
 
 const Hero = () => {
   const handleScroll = () => {};
@@ -16,6 +16,11 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src={}></Image>
+        </div>
       </div>
     </div>
   );
