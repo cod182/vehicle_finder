f5bef665da8666537d9a42397eacffa6b0e7538b
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-20 15:30:39 +0100

    created interface for custombutton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+'use client';
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+import { CustomButtonProps } from '@Types';
+import Image from 'next/image';
+import { MouseEventHandler } from 'react';
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custom-btn`}
+      onClick={() => {}}
+    >
+      <span className={`flex-1`}>title</span>
+    </button>
+  );
 };
 
 export default CustomButton;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,21 @@
+'use client';
+
 import image from 'next/image';
 
 import { CustomButton } from '@components';
 
 const Hero = () => {
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">Find | Book | Rent - Quick and easy</h1>
         <p className="hero__subtitle">streamline your car rental experience</p>
-        <CustomButton />
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
     </div>
   );
