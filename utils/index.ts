50f7967c405e7b2291b9156b33a85069ba27e4ed
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-21 17:48:56 +0100

    modified api to fethc all cars. accepts params

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,13 @@
-import { CarProps } from '@Types';
+import { CarProps, FilteredProps } from '@Types';
 
 export async function fetchCars(filters: FilteredProps) {
+  const { manufacturer, model, year, fuel, limit } = filters;
   const headers = {
     'X-RapidAPI-Key': process.env.RAPID_API_KEY,
     'X-RapidAPI-Host': process.env.RAPID_API_HOST,
   };
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=defender',
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&model=${model}&year=${year}&fuel_type=${fuel}&limit=${limit}`,
     { headers: headers }
   );
 
