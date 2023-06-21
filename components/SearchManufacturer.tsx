cbb80f1f6740e99fac44a07c2612149342381a48
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-21 12:12:06 +0100

    added manufacturer search styl

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -4,12 +4,24 @@ import { SearchManufacturerProps } from '@Types';
 import { Combobox, Transition } from '@headlessui/react';
 import Image from 'next/image';
 import { useState, Fragment } from 'react';
+import { manufacturers } from '@/constants';
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
   const [query, setQuery] = useState('');
+
+  const filteredManufacturers =
+    query === ''
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
+        );
+
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -29,19 +41,40 @@ const SearchManufacturer = ({
             displayValue={(manufacturer: string) => manufacturer}
             onChange={(e) => setQuery(e.target.value)}
           />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.length === 0 && query != '' ? (
+                <Combobox.Option
+                  value="Query"
+                  className="search-manufacturer__option"
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    value={item}
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                      }`
+                    }
+                  >
+                    {item}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
-      <Transition
-        as={Fragment}
-        leave="transition ease-in duration-100"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-        afterLeave={() => setQuery('')}
-      >
-        <Combobox.Options>
-          {}
-        </Combobox.Options>
-      </Transition>
     </div>
   );
 };
