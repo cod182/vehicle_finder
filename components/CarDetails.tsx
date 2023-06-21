9ab80b4fbbd74c423a0fba3229d9f9fe6c859421
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-21 16:01:31 +0100

    added car images with placebolders

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -38,8 +38,12 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel>
-                  <button type="button" onClick={closeModal}>
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
+                  <button
+                    type="button"
+                    onClick={closeModal}
+                    className="absolute top-2 right-2 z-[10] w-fit p-2 bg-primary-blue-100 rounded-full"
+                  >
                     <Image
                       src="/close.svg"
                       alt="close"
@@ -48,6 +52,51 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       className="object-contain"
                     />
                   </button>
+                  <div className="flex-1 flex flex-col gap-3">
+                    {/* Large Car Image */}
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        src="/hero.png"
+                        alt="car model"
+                        fill
+                        priority
+                        className="object-contain"
+                      />
+                    </div>
+                    {/* Small Car Images */}
+                    <div className="flex gap-3">
+                      {/* Small Car Image 1 */}
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src="/hero.png"
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      {/* Small Car Image 2 */}
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src="/hero.png"
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      {/* Small Car Image 3 */}
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src="/hero.png"
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
