a5eb79452a7b25d85734ff6e7a0ecf5446902ed8
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-21 17:26:27 +0100

    created the submit button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,24 @@
 'use client';
+
 import { useState } from 'react';
+import Image from 'next/image';
 
 import { SearchManufacturer } from './';
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
+
+  const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+    <button type="submit" className={`-ml-3 z-[10] ${otherClasses}`}>
+      <Image
+        src="/magnifying-glass.svg"
+        alt="magnifying glass"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </button>
+  );
 
   const handleSearch = () => {};
   return (
@@ -14,6 +28,7 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
     </form>
   );
