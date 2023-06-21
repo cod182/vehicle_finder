8fa2c68cb400b07d140d6838bb48628e0abe0726
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-21 10:48:20 +0100

    started the catalogue of cars area

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 p-x p-y max-width" id="discover">
+        
+      </div>
     </main>
   );
 }
