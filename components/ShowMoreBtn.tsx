8094df782821b82f88c19541ca6e79fb809332f8
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-22 12:27:59 +0100

    set up handle show more to increase limit

--- components/ShowMoreBtn.tsx
+++ components/ShowMoreBtn.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 import { ShowMoreBtnProps } from '@Types';
 import { CustomButton } from './';
+import { UpdateSearchParams } from '@utils';
 
 const ShowMoreBtn = ({ pageNumber, isNext }: ShowMoreBtnProps) => {
   const router = useRouter();
-  const handleNavigation = () => {};
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+
+    const newPathName = UpdateSearchParams('limit', newLimit.toString());
+
+    router.push(newPathName);
+  };
 
   return (
     <div className="w-full flex-center gap-5 mt-10">
