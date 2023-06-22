efadde1aa02d223e9a0ce7c09784b5cb4d8e5e04
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-06-22 12:23:22 +0100

    btnadded to show more

--- components/ShowMoreBtn.tsx
+++ components/ShowMoreBtn.tsx
@@ -1,7 +1,25 @@
+'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import { ShowMoreBtnProps } from '@Types';
+import { CustomButton } from './';
 
-const ShowMoreBtn = () => {
-  return <div>ShowMoreBtn</div>;
+const ShowMoreBtn = ({ pageNumber, isNext }: ShowMoreBtnProps) => {
+  const router = useRouter();
+  const handleNavigation = () => {};
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ShowMoreBtn;
